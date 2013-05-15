83daab8217942fc2c52e1d322ba38f458515998d
Willy Wonka <akhlaq87@gmail.com>
2013-05-15 19:51:26 +0200

    added the sidemenu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,24 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+// ------- Snap js Function
+$(document).ready(function() {
+    var snapper = new Snap({
+        element: document.getElementById('content'),
+        disable: 'right'
+    });
+
+    var addEvent = function addEvent(element, eventName, func) {
+    if (element.addEventListener) {
+        return element.addEventListener(eventName, func, false);
+    } else if (element.attachEvent) {
+        return element.attachEvent("on" + eventName, func);
+    }
+    };
+
+    addEvent(document.getElementById('open-left'), 'click', function(){
+        snapper.open('left');
+    });
+
+});
