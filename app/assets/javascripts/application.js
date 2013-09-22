5ac2fa18a9043f56b376a5159fc9a661d048263a
Wonka <akhlaq87@gmail.com>
2013-09-22 16:04:35 +0200

    deleted the swipe js file and readded the snap js function in appplication.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,23 @@
 
 
 
+// ------- Snap js Function
+$(document).ready(function() {
+    var snapper = new Snap({
+        element: document.getElementById('content'),
+        disable: 'right'
+    });
+
+    var addEvent = function addEvent(element, eventName, func) {
+    if (element.addEventListener) {
+        return element.addEventListener(eventName, func, false);
+    } else if (element.attachEvent) {
+        return element.attachEvent("on" + eventName, func);
+    }
+    };
+
+    addEvent(document.getElementById('open-left'), 'click', function(){
+        snapper.open('left');
+    });
+
+});
