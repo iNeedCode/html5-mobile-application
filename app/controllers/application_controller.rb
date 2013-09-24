ecbaecf3c138ffdb8026c8368b409f3b42c5ac8a
Wonka <akhlaq87@gmail.com>
2013-09-24 23:22:51 +0200

    added the group in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,24 @@
 
 class ApplicationController < ActionController::Base
 	protect_from_forgery
-	helper_method :all_regions, :soccer_rounds, :available_places
+	helper_method :all_regions, :soccer_rounds, :available_places, :available_groups
 
 
 	def all_regions
-		@all_regions = ["Muqami", "Nord", "Nord-Ost", "Nord-West", "Süd", "Süd-Ost", "Süd-West", "West", "Mitte", "Ost"]
+		all_regions = ["Muqami", "Nord", "Nord-Ost", "Nord-West", "Süd", "Süd-Ost", "Süd-West", "West", "Mitte", "Ost"]
 	end
 
 	def soccer_rounds
-		@soccer_rounds = ["Vorrunde", "Achtelfinale", "Viertelfinale", "Halbfinale", "Spiel um Platz 3", "Finale"]
+		soccer_rounds = ["Vorrunde", "Achtelfinale", "Viertelfinale", "Halbfinale", "Spiel um Platz 3", "Finale"]
 	end
 
 	def available_places
-		@available_places = ["Fußball A", "Fußball B", "Cricket A", "Cricket B"]
+		available_places = ["Fußball A", "Fußball B", "Cricket A", "Cricket B"]
+	end
+
+	def available_groups
+		groups = "A".."G"
+		groups.to_a
 	end
 
 end
